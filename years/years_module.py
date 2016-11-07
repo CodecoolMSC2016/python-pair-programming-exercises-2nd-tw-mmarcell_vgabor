f2ac78d95cbff7a8f9262c59e3d872ba45800e09
Gábor Varga <gabor.varga@suprasoft.hu>
2016-11-07 16:57:01 +0100

    Done

--- years/years_module.py
+++ years/years_module.py
@@ -2,12 +2,15 @@ import datetime
 
 
 def years(age):
-    return
+    today = datetime.date.today()
+    hundredyears = int(today.year) + (100 - age)
+    return hundredyears
 
 
 def main():
-    return
-
+    name = input("Name: ")
+    age = int(input("Age: "))
+    print("Hi,", name, "You will be 100 old in", years(age))
 
 if __name__ == '__main__':
     main()
